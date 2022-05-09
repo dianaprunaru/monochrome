3895f2a1638ac379e4e8d8acea7852b3c6bbf949
dianaprunaru <mosoianudiana@gmail.com>
2022-05-09 14:58:19 +0300

    monochrome_first steps

--- pages/components/header.jsx
+++ pages/components/header.jsx
@@ -1,7 +1,30 @@
 export default function Header() {
   return (
     <header className="header">
-h
+      <div className="header-top">
+      <section className="header-primary d-lg-none">
+
+      </section>
+
+      <section className="header-secondary">
+        <div className="header-connect d-none d-lg-block">
+
+        </div>
+
+        <div className="header-controls">
+
+        </div>
+      </section>
+
+      <section className="header-top-inner">
+
+      </section>
+      </div>
+
+      <div className="header-bottom">
+
+      </div>
+
     </header>
   );
 }
