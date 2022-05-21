067910e325ebf9a539902bfc752f44e4d23eda55
dianaprunaru <mosoianudiana@gmail.com>
2022-05-21 17:15:50 +0300

    monochrome_main_21mai

--- components/main.jsx
+++ components/main.jsx
@@ -1,3 +1,392 @@
 export default function Main() {
-  return <main>m</main>;
+  return (
+    <main className="content">
+      <section className="content-header d-block d-lg-none">
+        <div className="banner-main d-block d-lg-none">
+          <div className="banner-main-inner container">
+            <button className="New-in" type="button">
+              NEW IN
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className="content-main">
+        <section className="home-catalog-preview container">
+          <hr />
+          <header>
+            <h1 className="d-block d-lg-none">Spring collection</h1>
+            <h1 className="d-none d-lg-block">New Stuff</h1>
+          </header>
+          <section className="product-tiles row col-2 col-lg-4">
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+          </section>
+        </section>
+
+        <section className="home-catalog-preview container">
+          <hr />
+          <header>
+            <h1 className="d-block d-lg-none">Best reviewed</h1>
+            <h1 className="d-none d-lg-block">Most loved</h1>
+          </header>
+          <section className="product-tiles row col-2 col-lg-4">
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+          </section>
+        </section>
+
+        <section className="home-content container">
+          poza si text cu float
+        </section>
+
+        <section className="home-catalog-preview container">
+          <hr />
+          <header>
+            <h1>Men's</h1>
+          </header>
+          <section className="product-tiles row col-2 col-lg-4">
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+          </section>
+        </section>
+
+        <section className="home-catalog-preview container">
+          <hr />
+          <header>
+            <h1>Women's</h1>
+          </header>
+
+          <section className="product-tiles row col-2 col-lg-4">
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+
+            <article className="product-tile">
+              <header className="product-tile-header">
+                <div className="product-tile-image">
+                  <a href="" title="Monochrome">
+                    <img
+                      src="/images/Recently Viewed Thumbnail 2-2.png"
+                      alt="Monochrome watch"
+                    ></img>
+                  </a>
+                </div>
+
+                <div className="product-tile-favorite">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </header>
+
+              <div className="product-tile-heading"></div>
+
+              <div className="product-tile-pricing"></div>
+            </article>
+          </section>
+        </section>
+      </section>
+    </main>
+  );
 }
